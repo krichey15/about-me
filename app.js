b1c3438cbfa7f9abbb27e8d83e04565914f25aec
krichey15 <richey.kyle.i@gmail.com>
2017-05-25 22:32:41 -0700

    added first qustion with if/else

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict'
+
+var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
+
+if (name.toUpperCase === YES) {
+  console.log('user got the anser correct');
+  alert('I am SO glad you understood the hint.');
+} else if (name.toUpperCase === NO) {
+  console.log('user didn\'t see the hint');
+  alert('Wrong! Hint: See Hint');
+}else {
+  console.log('no answer');
+  alert('It is just three letters! Are you really SO lazy?');
+}
