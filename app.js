669edd0edaf1c313f090fe53921fa9c6b531821a
krichey15 <richey.kyle.i@gmail.com>
2017-05-30 18:22:58 -0700

    fixed uppercase bug

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
 
-if (name.toUpperCase === 'YES') {
+if (name.toUpperCase() === 'YES') {
   console.log('user got the anser correct');
   alert('I am SO glad you understood the hint.');
-} else if (name.toUpperCase === 'NO') {
+} else if (name.toUpperCase() === 'NO') {
   console.log('user didn\'t see the hint');
   alert('Wrong! Hint: See Hint');
 }else {
