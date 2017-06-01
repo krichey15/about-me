28f116cdb12698e6bd7e543896c6445f4e15889f
krichey15 <richey.kyle.i@gmail.com>
2017-05-31 18:17:54 -0700

    Restarted app.js with While Loops

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-'use strict'
+'use strict';
 
-var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
+var name;
+var origin;
+var age;
+var bae;
+var home;
 
-if (name.toUpperCase() === 'YES') {
-  console.log('user got the anser correct');
-  alert('I am SO glad you understood the hint.');
-} else if (name.toUpperCase() === 'NO') {
-  console.log('user didn\'t see the hint');
-  alert('Wrong! Hint: See Hint');
-}else {
-  console.log('no answer');
-  alert('It is just three letters! Are you really SO lazy?');
-}
-
-var age = prompt('Ok so I see you can read, but now it gets tough. \n\nHow old am I?');
-
-if (age === 28) {
-  console.log('They know my age');
-  alert('First Try!!');
-}else if (age > 28) {
-  console.log('user thinks I\'m old');
-  alert('I am not that old!!! Wow...just wow.');
+while (name.toUpperCase() !== 'YES') {
+  name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
 }
