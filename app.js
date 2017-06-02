dc36f26dc03607bc0f34c883734c0af128053b72
krichey15 <richey.kyle.i@gmail.com>
2017-06-01 19:48:00 -0700

    added more logic to first question

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ var home;
 
 while (name.toUpperCase() !== 'YES') {
   name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
+  if (name.toUpperCase() === 'YES') {
+    break;
+  }
+  alert('Hint: See Hint!')
 }
+console.log('Understood my hint')
