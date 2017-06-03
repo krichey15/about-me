c21ba7f1844810e49f6ae98e4dcea7a636df56d2
krichey15 <richey.kyle.i@gmail.com>
2017-06-03 09:56:14 -0700

    while loops for days

--- app.js
+++ app.js
@@ -5,12 +5,64 @@ var origin;
 var age;
 var bae;
 var home;
+var correct = 0;
 
-while (name.toUpperCase() !== 'YES') {
+while (name.toUpperCase() !== 'YES' || 'Y') {
   name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
-  if (name.toUpperCase() === 'YES') {
+  if (name.toUpperCase() === 'YES' || 'Y') {
     break;
   }
-  alert('Hint: See Hint!')
+  alert('Hint: See Hint!');
+  console.log('More than one try.')
 }
-console.log('Understood my hint')
+correct++;
+console.log('Understood my hint');
+console.log(correct);
+
+while (origin.toUpperCase() !== 'YES' || 'Y') {
+  origin = prompt('')
+  if (orign.toUpperCase() === 'YES' || 'Y') {
+    break;
+  }
+  alert();
+  console.log();
+}
+correct++
+console.log();
+console.log();
+
+while (origin.toUpperCase() !== 'YES' || 'Y') {
+  origin = prompt('')
+  if (orign.toUpperCase() === 'YES' || 'Y') {
+    break;
+  }
+  alert();
+  console.log();
+}
+correct++
+console.log();
+console.log();
+
+while (origin.toUpperCase() !== 'YES' || 'Y') {
+  origin = prompt('')
+  if (orign.toUpperCase() === 'YES' || 'Y') {
+    break;
+  }
+  alert();
+  console.log();
+}
+correct++
+console.log();
+console.log();
+
+while (origin.toUpperCase() !== 'YES' || 'Y') {
+  origin = prompt('')
+  if (orign.toUpperCase() === 'YES' || 'Y') {
+    break;
+  }
+  alert();
+  console.log();
+}
+correct++
+console.log();
+console.log();
