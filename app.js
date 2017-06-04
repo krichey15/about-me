d7247571f7748b642382fd4bb772142a1f32958d
krichey15 <richey.kyle.i@gmail.com>
2017-06-03 22:29:30 -0700

    Document writing

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 var name;
 var correct = 0;
 
-document.write('To Recap:');
 
 while (name.toUpperCase() !== 'YES' || name.toUpperCase() !== 'Y') {
   name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
@@ -12,7 +11,6 @@ while (name.toUpperCase() !== 'YES' || name.toUpperCase() !== 'Y') {
     alert(correct + ' of 5 correct.');
     console.log('Question 1: Understood my hint');
     console.log('correct: ' + correct);
-    document.write('My name is Kyle.');
     break;
   } else if (name.toUpperCase() === 'NO' || name.toUpperCase() === 'N') {
     alert('Hint: See Hint!');
@@ -32,7 +30,6 @@ if (island.toUpperCase() === 'YES' || island.toUpperCase() === 'Y') {
   correct++;
   alert(correct + ' of 5 correct.');
   console.log('correct: ' + correct);
-  document.write('I grew up on Camano Island.');
 } else {
   alert('Seriously? \"Yes\" and \"No\" are not difficult words.');
   console.log('Question 2: Not a \"Yes\" or \"No\" answer.');
@@ -44,7 +41,6 @@ if (age.toUpperCase() === 'YES' || age.toUpperCase() === 'Y') {
   correct++;
   alert('Yup and my birthday is August 27th.\n\nI expect birthday wishes \;\)\n\n' + correct + ' of 5 correct.');
   console.log('correct: ' + correct);
-  document.write('I am 28 years old.')
 } else if (age.toUpperCase() === 'NO' || age.toUpperCase() === 'N'){
   alert ('Rude');
   console.log('Question 3: Wrong');
@@ -61,23 +57,23 @@ while (wife.toUpperCase() !== 'YES' || wife.toUpperCase() !== 'Y') {
     correct++;
     alert('Never forget what was said by you today.\n\n' + correct + ' of 5 correct.');
     console.log('correct: ' + correct);
-    document.write('My wife is the best. Her name is Courtney.')
     break;
   } else if (wife.toUpperCase() === 'NO' || wife.toUpperCase() === 'N') {
     alert('You know what you have to say.\n\n\(I don\'t care if you don\'t have a wife\)');
     console.log('Question 4: More than one try.');
+    wife = prompt('Is my wife better than your wife?');
   } else {
     alert('Seriously? \"Yes\" and \"No\" are not difficult words.');
     console.log('Question 4: Not a \"Yes\" or \"No\" answer.');
+    wife = prompt('Is my wife better than your wife?');
   }
 }
 
-var home = prompt('Do I get to call the Kitsap Peninsula my home?')
+var home = prompt('Do I get to call the Kitsap Peninsula my home?');
 
 if (home.toUpperCase() === 'YES' || home.toUpperCase() === 'Y') {
   correct++;
   alert('Yup my wife and I bought a home in the beautiful small town of Kingston.\n\n' + correct + ' of 5 correct.')
-  document.write('I live in Kingston, WA.')
 } else if (home.toUpperCase() === 'NO' || home.toUpperCase() === 'N'){
   alert('I know it is far but I love it over there and it is in fact where I live.');
   console.log('Question 5: Wrong');
@@ -86,8 +82,26 @@ if (home.toUpperCase() === 'YES' || home.toUpperCase() === 'Y') {
   console.log('Question 5: Not a \"Yes\" or \"No\" answer.');
 }
 
+
 if (correct === 5){
+  document.write('<br>');
   document.write('PERFECT SCORE! CONGRATULATIONS!');
+  document.write('<br>')
 }else {
-  document.write('Womp Womp! You could always refresh the page and try again.');
+  document.write('<br>');
+  document.write('Womp Womp! only ' + corrrect + 'of 5 correct. You could always refresh the page and try again.');
+  document.write('<br>');
 }
+
+document.write('To Recap:')
+document.write('<br>');
+document.write('My name is Kyle.');
+document.write('<br>')
+document.write('I live in Kingston, WA.');
+document.write('<br>');
+document.write('I grew up on Camano Island.');
+document.write('<br>');
+document.write('I am 28 years old.');
+document.write('<br>');
+document.write('My wife is the best. Her name is Courtney.');
+document.write('<br>');
