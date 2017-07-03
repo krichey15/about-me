3a7bc6652e42a075dceb2046735476de0aabd75e
Kyle Richey <richey.kyle.i@gmail.com>
2017-07-02 18:23:36 -0700

    commented out list and fixed linter errors

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ function myName(){
   var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
   while (name.toUpperCase() !== 'YES' || name.toUpperCase() !== 'Y') {
     if (name.toUpperCase() === 'YES' || name.toUpperCase() === 'Y') {
-    correct++;
-    alert(correct + ' of 5 correct.');
-    console.log('Question 1: Understood my hint');
-    console.log('correct: ' + correct);
-    break;
+      correct++;
+      alert(correct + ' of 5 correct.');
+      console.log('Question 1: Understood my hint');
+      console.log('correct: ' + correct);
+      break;
     } else if (name.toUpperCase() === 'NO' || name.toUpperCase() === 'N') {
-    alert('Hint: See Hint!');
-    console.log('Question 1: More than one try.');
-    var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
+      alert('Hint: See Hint!');
+      console.log('Question 1: More than one try.');
+      name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
     } else {
-    alert('Seriously? \"Yes\" and \"No\" are not difficult words.');
-    console.log('Question 1: Not a \"Yes\" or \"No\" answer.');
-    var name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
+      alert('Seriously? \"Yes\" and \"No\" are not difficult words.');
+      console.log('Question 1: Not a \"Yes\" or \"No\" answer.');
+      name = prompt('This is the first of five YES or NO questions to unlock my life. \nHere is is a gimme\'. \n\nIs my name Kyle?\n\n\(Hint:YES\)');
     }
   }
 }
@@ -67,11 +67,11 @@ function betterWife(){
     } else if (wife.toUpperCase() === 'NO' || wife.toUpperCase() === 'N') {
       alert('You know what you have to say.\n\n\(I don\'t care if you don\'t have a wife\)');
       console.log('Question 4: More than one try.');
-      var wife = prompt('Is my wife better than your wife?');
+      wife = prompt('Is my wife better than your wife?');
     } else {
       alert('Seriously? \"Yes\" and \"No\" are not difficult words.');
       console.log('Question 4: Not a \"Yes\" or \"No\" answer.');
-      var wife = prompt('Is my wife better than your wife?');
+      wife = prompt('Is my wife better than your wife?');
     }
   }
 }
@@ -92,18 +92,18 @@ function kitsap(){
   }
 }
 
-if (correct === 5){
-  document.write('<br>');
-  document.write('PERFECT SCORE! CONGRATULATIONS!');
-  document.write('<br>')
-}else {
-  document.write('<br>');
-  document.write('Womp Womp! only ' + correct + ' of 5 correct. You could always refresh the page and try again.');
-  document.write('<br>');
-}
-
 myName();
 whichIsland();
 howOld();
 betterWife();
 kitsap();
+
+if (correct === 5){
+  document.write('<br>');
+  document.write('PERFECT SCORE! CONGRATULATIONS!');
+  document.write('<br>');
+}else {
+  document.write('<br>');
+  document.write('Womp Womp! only ' + correct + ' of 5 correct. You could always refresh the page and try again.');
+  document.write('<br>');
+}
